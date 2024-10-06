5b901e72bfc147c24c9be3f46417bae6f20a6431
Cezn.Vi <cezn.vi@gmail.com>
2024-10-06 09:32:51 +0300

    fix EmailVerificationRepository bug with select data from DB (userId)

--- spring/src/main/java/org/booking/spring/repositories/EmailVerificationRepository.java
+++ spring/src/main/java/org/booking/spring/repositories/EmailVerificationRepository.java
@@ -2,9 +2,12 @@ package org.booking.spring.repositories;
 
 import org.booking.spring.models.user.EmailVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+
+    @Query("SELECT ev FROM EmailVerification ev WHERE ev.userId = :userId")
     EmailVerification findByUserId(Long userId);
 }
