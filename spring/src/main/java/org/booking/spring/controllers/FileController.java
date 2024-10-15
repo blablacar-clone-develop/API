c9242d12b22b8d1172edea1b2c24bab9b6f4f743
Cezn.Vi <cezn.vi@gmail.com>
2024-10-15 15:23:20 +0300

    fix url linking

--- spring/src/main/java/org/booking/spring/controllers/FileController.java
+++ spring/src/main/java/org/booking/spring/controllers/FileController.java
@@ -65,6 +65,7 @@ public class FileController {
             // Викликаємо метод збереження файлу
             String filePath = storageService.put("avatar", fileName, file);
 
+
             String[] adressArr = filePath.split("9000");
             if(adressArr[0].equals("http://blablacar.imageStorage.minio:")) {
                 filePath = "/storage"+ adressArr[1];
